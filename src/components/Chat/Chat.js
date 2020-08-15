b099870a9ef5e4230d1e918997117923d8a50093
aalmeida00 <andre-silva78@hotmail.com>
2020-08-15 15:19:48 -0300

    Mudanças no chat para pegar as informações do banco de dados

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Chat.css";
 import {useParams} from "react-router-dom";
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import db from "../../firebase"
 
 function Chat() {
     const { roomId } = useParams();
+    const [roomDetails, setRoomDetails] = useState(null);
+    const [roomMessages, setRoomMessages] = useState(null);
+
+    useEffect(()=>{
+        if (roomId){
+            db.collection('rooms')
+            .doc(roomId)
+            .onSnapshot(snapshot => (
+                setRoomDetails(snapshot.data())
+            ))
+        }
+
+        db.collection('rooms')
+        .doc(roomId)
+        .collection('messages')
+        .orderBy('timestamp', 'asc')
+        .onSnapshot(
+           snapshot => setRoomMessages(
+                snapshot.docs.map(doc => doc.data())
+            )
+        )
+
+
+    }, [roomId])
+
+    
+    console.log(roomDetails)
+    console.log("Messagess >>", roomMessages)
+
     return (
         <div className="chat">
             <div className="chat__header">
                 <div className="chat__headerLeft">
                     <h4 className="chat__channelName">
-                        <strong># General</strong>
+                        <strong># {roomDetails?.name}</strong>
                         <StarBorderOutlinedIcon />
                     </h4>
                 </div>
